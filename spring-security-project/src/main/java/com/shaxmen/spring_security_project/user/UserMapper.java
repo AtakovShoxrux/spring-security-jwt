3c606d697f78d769d520f2b54760d63aca904959
Shoxrux <atakovshoxrux@gmail.com>
2025-09-16 12:20:02 +0500

    Got it running in terminal

--- spring-security-project/src/main/java/com/shaxmen/spring_security_project/user/UserMapper.java
+++ spring-security-project/src/main/java/com/shaxmen/spring_security_project/user/UserMapper.java
@@ -7,9 +7,9 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.springframework.stereotype.Service;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
   @Mapping(target = "firstName", source = "firstName")
